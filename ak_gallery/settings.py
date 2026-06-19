39bf36618d02c503a299db48e651d97b61a1961c
nanochka-8000 <israilova.aliya13@gmail.com>
2026-06-19 12:17:21 +0600

    справила пути для медиафайлов и настроила отображение фото

--- ak_gallery/settings.py
+++ ak_gallery/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 import os
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'info@akgallery.kg'
